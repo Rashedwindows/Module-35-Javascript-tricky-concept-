085d069957a0ca4396c6d031db44ecfcac795677
Rashedwindows <mdhabiburrahmanrashed@gmail.com>
2022-03-31 03:32:31 +0600

    added scope

---
+++ scope.js
@@ -0,0 +1,17 @@
+const favNum = 27;
+
+function add(first,second){
+
+    const result = first + second;
+    if(result > 9){
+
+        const mood = 'happy';
+        
+    }
+    return result;
+   
+}
+
+const sum = add(11,35);
+
+console.log(mood)
